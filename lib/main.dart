4ddfee01d206c654ff536e413172af1a17d367b7
Oluwatuyi Abiola <abiolaoluwatuyi@gmail.com>
2022-06-08 23:19:26 +0100

    A dice flutter application from londonappbrewery

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black54,
+        appBar: AppBar(
+          title: const Text('Dicee'),
+          backgroundColor: Colors.black54,
+        ),
+        body: const DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 1;
+  var rightDiceNumber = 1;
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: TextButton(
+                  onPressed: () {
+                    changeDiceFace();
+                  },
+                  child: Image.asset('images/dice$leftDiceNumber.png')),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  changeDiceFace();
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
